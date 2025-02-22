3435981386faea5e966ba2e5a87990b150fa8f8b
Satyam Kumar <satyamsks999000@gmail.com>
2025-02-22 12:38:55 +0530

    extra files add ki h

---
+++ helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<" mai uper aasma niche";
+    return 0;
+}
